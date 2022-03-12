0a80c322626a1c4ca70a2260009e397c4819a902
yjx <2509693235@qq.com>
2022-03-12 10:43:07 +0800

    Initial commit

---
+++ src/test/java/com/example/gittestdemo/GittestdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gittestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittestdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
